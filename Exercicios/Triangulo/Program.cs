6db202cd538b9c9f277ea8897b4d444e0f557361
Willian Oliveira(Unisys) <Willian.Oliveira@unisys.com>
2020-01-30 15:50:26 -0300

    primeiro commit

---
+++ Exercicios/Triangulo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lado1, lado2, lado3;
+            Console.Write("Digite o primeiro lado do triangulo");
+            lado1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite o segundo lado do triangulo");
+            lado2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite o terceiro lado do triangulo");
+            lado3 = Convert.ToInt32(Console.ReadLine());
+
+
+        }
+    }
+}
