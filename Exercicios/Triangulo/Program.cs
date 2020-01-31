6ef28012597ebcf3b2dfad6ce92ed96c59f6474e
Willian Oliveira(Unisys) <Willian.Oliveira@unisys.com>
2020-01-31 10:10:57 -0300

    Erro na logica

--- Exercicios/Triangulo/Program.cs
+++ Exercicios/Triangulo/Program.cs
@@ -10,6 +10,13 @@ namespace Triangulo
     {
         static void Main(string[] args)
         {
+        /*Faça um Programa que peça os 3 lados de um triângulo. O programa deverá informar se os valores podem ser um triângulo. Indique, caso os lados formem um triângulo, se o mesmo é: equilátero, isósceles ou escaleno.
+        Dicas:
+            Três lados formam um triângulo quando a soma de quaisquer dois lados for maior que o terceiro;
+            Triângulo Equilátero: três lados iguais;
+            Triângulo Isósceles: quaisquer dois lados iguais;
+            Triângulo Escaleno: três lados diferentes
+          */
             double lado1, lado2, lado3;
             Console.Write("Digite o primeiro lado do triangulo: ");
             lado1 = Convert.ToDouble(Console.ReadLine());
