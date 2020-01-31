32d2205868d2c65b08967c665961bda3c13fe078
Willian Oliveira(Unisys) <Willian.Oliveira@unisys.com>
2020-01-31 11:19:20 -0300

    primeiro commit

---
+++ Exercicios/TesteLoginIgualSenha/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteLoginIgualSenha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean passa = false;
+            while (passa == false)
+            {
+                Console.WriteLine("***Cadastro de Usuario****");
+                Console.Write("Digite um nome de Usuario: ");
+                string usuario = Console.ReadLine();
+                Console.Write("Digite uma senha: ");
+                string senha = Console.ReadLine();
+                Console.WriteLine("");
+
+                if (usuario == senha)
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Erro 6969 - Usuario e Senha iguais.");
+                    Console.WriteLine("Por favor tente novamente.");
+                }
+                else
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Cadastro efetuado com sucesso");
+                    passa = true;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
