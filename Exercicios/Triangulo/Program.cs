acb80941d8a01acb7b844d95bbd6ba577f0e6022
Willian Oliveira(Unisys) <Willian.Oliveira@unisys.com>
2020-01-31 10:10:02 -0300

    Erro no código

--- Exercicios/Triangulo/Program.cs
+++ Exercicios/Triangulo/Program.cs
@@ -10,15 +10,30 @@ namespace Triangulo
     {
         static void Main(string[] args)
         {
-            int lado1, lado2, lado3;
-            Console.Write("Digite o primeiro lado do triangulo");
-            lado1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Digite o segundo lado do triangulo");
-            lado2 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Digite o terceiro lado do triangulo");
-            lado3 = Convert.ToInt32(Console.ReadLine());
+            double lado1, lado2, lado3;
+            Console.Write("Digite o primeiro lado do triangulo: ");
+            lado1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite o segundo lado do triangulo: ");
+            lado2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite o terceiro lado do triangulo: ");
+            lado3 = Convert.ToDouble(Console.ReadLine());
+            // erro na lógica quando n formar um triangulo
 
-
+            if ((lado2 - lado3 < lado1) && (lado1 < lado2 + lado3) && (lado1 - lado3 < lado2) && (lado2 < lado1 + lado3) && (lado1 - lado2 < lado3) && (lado3 < lado1 + lado2))
+            {
+                Console.WriteLine("Os lados não formam um triangulo!!!");
+            }
+            /*else if(lado1 == lado2 && lado1== lado3)
+            {
+                Console.WriteLine("Seus lados formam um Triangulo EQUILÁTERO");
+            }else if(lado1 == lado2 || lado1 == lado3 || lado2 == lado3)
+            {
+                Console.WriteLine("Seus lados formam um Triangulo ISÓSCELES");
+            }
+            else{
+                Console.WriteLine("Seus lados formam um Triangulo ESCALENO");
+            }*/
+            Console.ReadKey();
         }
     }
 }
