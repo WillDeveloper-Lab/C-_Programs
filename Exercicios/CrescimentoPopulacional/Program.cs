13561d27782d551d8f6a74c879b326384e812520
Willian Oliveira(Unisys) <Willian.Oliveira@unisys.com>
2020-02-03 09:37:14 -0300

    Adicionando regras de negócio na class Program

--- Exercicios/CrescimentoPopulacional/Program.cs
+++ Exercicios/CrescimentoPopulacional/Program.cs
@@ -10,6 +10,22 @@ namespace CrescimentoPopulacional
     {
         static void Main(string[] args)
         {
+            double populacaoA = 80000;
+            double populacaoB = 200000;
+            int anos;
+            int i = 1;
+            for (anos = 0; anos < i; anos++)
+            {
+                if (populacaoB >= populacaoA)
+                {
+                    populacaoA += populacaoA * 0.03;
+                    populacaoB += populacaoB * 0.015;
+                    i++;
+                }
+            }
+            Console.WriteLine($"A = {populacaoA} - B = {populacaoB}");
+            // Console.WriteLine($"Foram necessarios {anos} anos para que a população A chegasse a: {populacaoA}");
+            Console.ReadKey();
         }
     }
 }
