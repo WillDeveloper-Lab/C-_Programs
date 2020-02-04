3fbe770dd13abeda11a107232b32ecff38a61bf8
Willian Oliveira(Unisys) <Willian.Oliveira@unisys.com>
2020-02-04 10:37:40 -0300

    primeiro commit

---
+++ Exercicios/Fibonacci/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um numero: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int anterior = 0;
+            int atual = 1;
+            Console.Write(anterior);
+            Console.Write(atual);
+            for (int i = 2; 2<=num; i++)
+            {
+                anterior = atual;
+                atual += anterior;
+                Console.WriteLine(atual);
+            }
+            Console.ReadKey();
+        }
+    }
+}
