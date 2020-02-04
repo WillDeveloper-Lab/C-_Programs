04dd6a07f4988bd77128d7ed680d34d3de9bb0d9
Willian Oliveira(Unisys) <Willian.Oliveira@unisys.com>
2020-02-04 09:45:57 -0300

    primeiro commit

---
+++ Exercicios/PotenciaSemFuncao/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotenciaSemFuncao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 3;
+            int calcula = a;
+            for(int i = 1; i <= b; i++)
+            {
+                calcula *= a;
+                Console.WriteLine(calcula);
+            }
+            Console.ReadKey();
+        }
+    }
+}
