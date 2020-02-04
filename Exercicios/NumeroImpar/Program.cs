c251e8b8897966e41a30dc494ba2214d7d524ee4
Willian Oliveira(Unisys) <Willian.Oliveira@unisys.com>
2020-02-04 10:04:34 -0300

    primeiro commit

---
+++ Exercicios/NumeroImpar/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumeroImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um numero: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                if( i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
