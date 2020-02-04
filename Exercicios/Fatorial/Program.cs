1704896d4e6c3cadb65df3390df77f656d7dc530
Willian Oliveira(Unisys) <Willian.Oliveira@unisys.com>
2020-02-04 09:38:32 -0300

    primeiro commit

---
+++ Exercicios/Fatorial/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fatorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fatorial = 5;
+            for(int i = fatorial -1; i>=1; i--)
+            {
+                fatorial *= i;
+                Console.WriteLine(fatorial);
+            }
+            Console.ReadKey();
+        }
+    }
+}
