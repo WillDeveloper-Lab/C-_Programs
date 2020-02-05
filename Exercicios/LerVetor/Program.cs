9e586c3a8faeb13ff4a2bfe32a1609ff52990fcc
Willian Oliveira(Unisys) <Willian.Oliveira@unisys.com>
2020-02-05 10:48:03 -0300

    primeiro commit

---
+++ Exercicios/LerVetor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LerVetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int [] num = new int[5] {1,2,3,4,5};
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(num[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
