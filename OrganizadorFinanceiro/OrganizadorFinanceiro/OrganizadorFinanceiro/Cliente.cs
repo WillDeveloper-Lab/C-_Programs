e4bcd661de8aef6993c238fb6d2a1164ab55e01f
Willian Oliveira(Unisys) <Willian.Oliveira@unisys.com>
2020-02-06 09:22:13 -0300

    Adicionando Atributos na class Cliente.

---
+++ OrganizadorFinanceiro/OrganizadorFinanceiro/OrganizadorFinanceiro/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganizadorFinanceiro
+{
+    class Cliente
+    {
+        public long cpf { get; set; }
+        public string nome { get; set; }
+        public string email { get; set; }
+        public int telefone { get; set; }
+        public int dataNascimento { get; set; }
+    }
+}
