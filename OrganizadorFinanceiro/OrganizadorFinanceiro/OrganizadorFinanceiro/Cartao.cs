991f54f811eba1eb547f6f866190628afbf0a4ce
Willian Oliveira(Unisys) <Willian.Oliveira@unisys.com>
2020-02-06 09:45:33 -0300

    Criando classe cartao

---
+++ OrganizadorFinanceiro/OrganizadorFinanceiro/OrganizadorFinanceiro/Cartao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganizadorFinanceiro
+{
+    class Cartao
+    {
+        public string apelido { get; set; }
+        public double limite { get; set; }
+
+    }
+}
